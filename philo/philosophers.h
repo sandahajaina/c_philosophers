303fbdf02056ca52be7c9a9bdf296aed90ac37c0
Sanda Hajaina Ranaivoarisoa <hajainasanda@gmail.com>
2024-07-29 12:50:32 +0300

    gitignore

---
+++ philo/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sranaivo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/29 11:37:41 by sranaivo          #+#    #+#             */
+/*   Updated: 2024/07/29 11:37:47 by sranaivo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+#endif
