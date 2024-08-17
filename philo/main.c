b995c789e92ed8c12d8a4408325ed8cc6f6c6c2c
Sanda Hajaina Ranaivoarisoa <hajainasanda@gmail.com>
2024-08-17 14:54:31 +0300

    starting from scratch

--- philo/main.c
+++ philo/main.c
@@ -6,17 +6,18 @@
 /*   By: sranaivo <sranaivo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 14:57:01 by sranaivo          #+#    #+#             */
-/*   Updated: 2024/08/16 15:06:00 by sranaivo         ###   ########.fr       */
+/*   Updated: 2024/08/17 14:52:29 by sranaivo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#include "philosophers.h"
+#include "philo.h"
 
 int main(int argc, char *argv[])
 {
     (void) argv;
-    if (argc == 4 || argc == 5)
+    t_rules rules;
+    if (argc == 5 || argc == 6)
     {
         printf("philo");
         return (0);
--- philo/philo.h
+++ philo/philo.h
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.h                                     :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sranaivo <sranaivo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/16 15:04:05 by sranaivo          #+#    #+#             */
-/*   Updated: 2024/08/16 15:04:09 by sranaivo         ###   ########.fr       */
+/*   Updated: 2024/08/17 14:52:33 by sranaivo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#ifndef PHILOSOPHERS_H
-# define PHILOSOPHERS_H
+#ifndef PHILO_H
+# define PHILO_H
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -20,6 +20,6 @@
 #include <sys/time.h>
 #include <pthread.h>
 
-//typedef struct philosopher
+
 
 #endif
